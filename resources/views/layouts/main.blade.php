72bb966c244eb63109f669f1995530a794a0e48a
Trivena Panjaitan <70930454+TrivenaPanjaitan@users.noreply.github.com>
2021-11-30 16:55:10 +0700

    Add icon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
     <title>Document</title>
+    <script src="{{ asset('js/a076d05399.js') }}" crossorigin='anonymous'></script>
 </head>
 
 <body>
