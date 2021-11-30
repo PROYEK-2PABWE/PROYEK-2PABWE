7a956b6f69b839733ca1603d6e972fbd9c05f440
ifs19017 <ifs19017@students.del.ac.id>
2021-12-01 00:58:42 +0700

    Update 3

--- resources/views/keluhan.blade.php
+++ resources/views/keluhan.blade.php
@@ -5,10 +5,10 @@
         <h2>Anda Memiliki Keluhan</h2>
         <p>Informasikan kepada kami, keluhan anda. Keluhan anda akan segera kami tindak lanjuti</p>
         <div class="w-75">
-            <div class="bg-secondary" style="width: 883px">
-                <i class="fas fa-pen"></i>
+            <div class="bg-secondary py-2">
+                <i class="fas fa-pen mx-3"></i>
             </div>
-            <textarea rows="10" cols="108.6">Tuliskan isi pesan anda</textarea>
+            <textarea rows="10" cols="120" style="max-width:100%">    Tuliskan isi pesan anda</textarea>
         </div>
         <input type="submit" value="Kirim" class="btn btn-primary bg-color-main py-3">
     </div>
