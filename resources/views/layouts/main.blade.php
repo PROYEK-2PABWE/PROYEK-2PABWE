bad1b8f9ec62ad0e27852d321370bc5d1a11ed04
Trivena Panjaitan <70930454+TrivenaPanjaitan@users.noreply.github.com>
2021-11-30 17:11:31 +0700

    Update header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
+    <link rel="stylesheet" href={{ asset('css/all.css') }}>
+    <link rel="stylesheet" href={{ asset('css/style.css') }}>
     <title>Document</title>
     <script src="{{ asset('js/a076d05399.js') }}" crossorigin='anonymous'></script>
 </head>
@@ -20,6 +22,9 @@
     @include('partials.footer')
 
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,4 @@
+<script src="{{ asset('js/TweenMax.min.js') }}"></script>
 <div class="super_container">
     <!-- Header -->
     <header class="header">
@@ -7,14 +8,10 @@
                 <div class="row">
                     <div class="col d-flex flex-row">
                         <div class="top_bar_contact_item">
-                            <div class="top_bar_icon"><img
-                                    src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918577/phone.png"
-                                    alt=""></div>+91 9823 132 111
+                            <div class="top_bar_icon"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918577/phone.png" alt=""></div>+91 9823 132 111
                         </div>
                         <div class="top_bar_contact_item">
-                            <div class="top_bar_icon"><img
-                                    src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918597/mail.png" alt="">
-                            </div><a href="mailto:fastsales@gmail.com">contact@bbbootstrap.com</a>
+                            <div class="top_bar_icon"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918597/mail.png" alt=""></div><a href="mailto:fastsales@gmail.com">contact@bbbootstrap.com</a>
                         </div>
                         <div class="top_bar_content ml-auto">
                             <div class="top_bar_menu">
@@ -36,9 +33,7 @@
                                 </ul>
                             </div>
                             <div class="top_bar_user">
-                                <div class="user_icon"><img
-                                        src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918647/user.svg"
-                                        alt=""></div>
+                                <div class="user_icon"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918647/user.svg" alt=""></div>
                                 <div><a href="#">Register</a></div>
                                 <div><a href="#">Sign in</a></div>
                             </div>
@@ -60,13 +55,9 @@
                         <div class="header_search">
                             <div class="header_search_content">
                                 <div class="header_search_form_container">
-                                    <form action="#" class="header_search_form clearfix"> <input type="search"
-                                            required="required" class="header_search_input"
-                                            placeholder="Search for products...">
+                                    <form action="#" class="header_search_form clearfix"> <input type="search" required="required" class="header_search_input" placeholder="Search for products...">
                                         <div class="custom_dropdown" style="display: none;">
-                                            <div class="custom_dropdown_list"> <span
-                                                    class="custom_dropdown_placeholder clc">All Categories</span> <i
-                                                    class="fas fa-chevron-down"></i>
+                                            <div class="custom_dropdown_list"> <span class="custom_dropdown_placeholder clc">All Categories</span> <i class="fas fa-chevron-down"></i>
                                                 <ul class="custom_list clc">
                                                     <li><a class="clc" href="#">All Categories</a></li>
                                                     <li><a class="clc" href="#">Computers</a></li>
@@ -76,10 +67,7 @@
                                                     <li><a class="clc" href="#">Smartphones</a></li>
                                                 </ul>
                                             </div>
-                                        </div> <button type="submit" class="header_search_button trans_300"
-                                            value="Submit"><img
-                                                src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918770/search.png"
-                                                alt=""></button>
+                                        </div> <button type="submit" class="header_search_button trans_300" value="Submit"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918770/search.png" alt=""></button>
                                     </form>
                                 </div>
                             </div>
@@ -88,9 +76,7 @@
                     <div class="col-lg-4 col-9 order-lg-3 order-2 text-lg-left text-right">
                         <div class="wishlist_cart d-flex flex-row align-items-center justify-content-end">
                             <div class="wishlist d-flex flex-row align-items-center justify-content-end">
-                                <div class="wishlist_icon"><img
-                                        src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918681/heart.png"
-                                        alt=""></div>
+                                <div class="wishlist_icon"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918681/heart.png" alt=""></div>
                                 <div class="wishlist_content">
                                     <div class="wishlist_text"><a href="#">Wishlist</a></div>
                                     <div class="wishlist_count">10</div>
@@ -98,9 +84,7 @@
                             </div> <!-- Cart -->
                             <div class="cart">
                                 <div class="cart_container d-flex flex-row align-items-center justify-content-end">
-                                    <div class="cart_icon"> <img
-                                            src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918704/cart.png"
-                                            alt="">
+                                    <div class="cart_icon"> <img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1560918704/cart.png" alt="">
                                         <div class="cart_count"><span>3</span></div>
                                     </div>
                                     <div class="cart_content">
@@ -124,8 +108,7 @@
                             <div class="main_nav_menu">
                                 <ul class="standard_dropdown main_nav_dropdown">
                                     <li><a href="#">Home<i class="fas fa-chevron-down"></i></a></li>
-                                    <li class="hassubs"> <a href="#">Laptop<i
-                                                class="fas fa-chevron-down"></i></a>
+                                    <li class="hassubs"> <a href="#">Laptop<i class="fas fa-chevron-down"></i></a>
                                         <ul>
                                             <li> <a href="#">Lenovo<i class="fas fa-chevron-down"></i></a>
                                                 <ul>
@@ -139,8 +122,7 @@
                                             <li><a href="#">HP<i class="fas fa-chevron-down"></i></a></li>
                                         </ul>
                                     </li>
-                                    <li class="hassubs"> <a href="#">Featured Brands<i
-                                                class="fas fa-chevron-down"></i></a>
+                                    <li class="hassubs"> <a href="#">Featured Brands<i class="fas fa-chevron-down"></i></a>
                                         <ul>
                                             <li> <a href="#">APPLE<i class="fas fa-chevron-down"></i></a>
                                                 <ul>
@@ -157,16 +139,12 @@
                                     <li class="hassubs"> <a href="#">Pages<i class="fas fa-chevron-down"></i></a>
                                         <ul>
                                             <li><a href="shop.html">Shop<i class="fas fa-chevron-down"></i></a></li>
-                                            <li><a href="product.html">Product<i class="fas fa-chevron-down"></i></a>
-                                            </li>
+                                            <li><a href="product.html">Product<i class="fas fa-chevron-down"></i></a></li>
                                             <li><a href="blog.html">Blog<i class="fas fa-chevron-down"></i></a></li>
-                                            <li><a href="blog_single.html">Blog Post<i
-                                                        class="fas fa-chevron-down"></i></a></li>
-                                            <li><a href="regular.html">Regular Post<i
-                                                        class="fas fa-chevron-down"></i></a></li>
+                                            <li><a href="blog_single.html">Blog Post<i class="fas fa-chevron-down"></i></a></li>
+                                            <li><a href="regular.html">Regular Post<i class="fas fa-chevron-down"></i></a></li>
                                             <li><a href="cart.html">Cart<i class="fas fa-chevron-down"></i></a></li>
-                                            <li><a href="contact.html">Contact<i class="fas fa-chevron-down"></i></a>
-                                            </li>
+                                            <li><a href="contact.html">Contact<i class="fas fa-chevron-down"></i></a></li>
                                         </ul>
                                     </li>
                                     <li><a href="blog.html">Blog<i class="fas fa-chevron-down"></i></a></li>
@@ -177,9 +155,7 @@
                                 <div class="menu_trigger d-flex flex-row align-items-center justify-content-end">
                                     <div class="menu_burger">
                                         <div class="menu_trigger_text">menu</div>
-                                        <div class="cat_burger menu_burger_inner">
-                                            <span></span><span></span><span></span>
-                                        </div>
+                                        <div class="cat_burger menu_burger_inner"><span></span><span></span><span></span></div>
                                     </div>
                                 </div>
                             </div>
@@ -194,12 +170,10 @@
                     <div class="col">
                         <div class="page_menu_content">
                             <div class="page_menu_search">
-                                <form action="#"> <input type="search" required="required"
-                                        class="page_menu_search_input" placeholder="Search for products..."> </form>
+                                <form action="#"> <input type="search" required="required" class="page_menu_search_input" placeholder="Search for products..."> </form>
                             </div>
                             <ul class="page_menu_nav">
-                                <li class="page_menu_item has-children"> <a href="#">Language<i
-                                            class="fa fa-angle-down"></i></a>
+                                <li class="page_menu_item has-children"> <a href="#">Language<i class="fa fa-angle-down"></i></a>
                                     <ul class="page_menu_selection">
                                         <li><a href="#">English<i class="fa fa-angle-down"></i></a></li>
                                         <li><a href="#">Italian<i class="fa fa-angle-down"></i></a></li>
@@ -207,8 +181,7 @@
                                         <li><a href="#">Japanese<i class="fa fa-angle-down"></i></a></li>
                                     </ul>
                                 </li>
-                                <li class="page_menu_item has-children"> <a href="#">Currency<i
-                                            class="fa fa-angle-down"></i></a>
+                                <li class="page_menu_item has-children"> <a href="#">Currency<i class="fa fa-angle-down"></i></a>
                                     <ul class="page_menu_selection">
                                         <li><a href="#">US Dollar<i class="fa fa-angle-down"></i></a></li>
                                         <li><a href="#">EUR Euro<i class="fa fa-angle-down"></i></a></li>
@@ -217,12 +190,10 @@
                                     </ul>
                                 </li>
                                 <li class="page_menu_item"> <a href="#">Home<i class="fa fa-angle-down"></i></a> </li>
-                                <li class="page_menu_item has-children"> <a href="#">Super Deals<i
-                                            class="fa fa-angle-down"></i></a>
+                                <li class="page_menu_item has-children"> <a href="#">Super Deals<i class="fa fa-angle-down"></i></a>
                                     <ul class="page_menu_selection">
                                         <li><a href="#">Super Deals<i class="fa fa-angle-down"></i></a></li>
-                                        <li class="page_menu_item has-children"> <a href="#">Menu Item<i
-                                                    class="fa fa-angle-down"></i></a>
+                                        <li class="page_menu_item has-children"> <a href="#">Menu Item<i class="fa fa-angle-down"></i></a>
                                             <ul class="page_menu_selection">
                                                 <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
                                                 <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
@@ -235,8 +206,7 @@
                                         <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
                                     </ul>
                                 </li>
-                                <li class="page_menu_item has-children"> <a href="#">Featured Brands<i
-                                            class="fa fa-angle-down"></i></a>
+                                <li class="page_menu_item has-children"> <a href="#">Featured Brands<i class="fa fa-angle-down"></i></a>
                                     <ul class="page_menu_selection">
                                         <li><a href="#">Featured Brands<i class="fa fa-angle-down"></i></a></li>
                                         <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
@@ -244,8 +214,7 @@
                                         <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
                                     </ul>
                                 </li>
-                                <li class="page_menu_item has-children"> <a href="#">Trending Styles<i
-                                            class="fa fa-angle-down"></i></a>
+                                <li class="page_menu_item has-children"> <a href="#">Trending Styles<i class="fa fa-angle-down"></i></a>
                                     <ul class="page_menu_selection">
                                         <li><a href="#">Trending Styles<i class="fa fa-angle-down"></i></a></li>
                                         <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
@@ -253,19 +222,15 @@
                                         <li><a href="#">Menu Item<i class="fa fa-angle-down"></i></a></li>
                                     </ul>
                                 </li>
-                                <li class="page_menu_item"><a href="blog.html">blog<i
-                                            class="fa fa-angle-down"></i></a></li>
-                                <li class="page_menu_item"><a href="contact.html">contact<i
-                                            class="fa fa-angle-down"></i></a></li>
+                                <li class="page_menu_item"><a href="blog.html">blog<i class="fa fa-angle-down"></i></a></li>
+                                <li class="page_menu_item"><a href="contact.html">contact<i class="fa fa-angle-down"></i></a></li>
                             </ul>
                             <div class="menu_contact">
                                 <div class="menu_contact_item">
-                                    <div class="menu_contact_icon"><img src="images/phone_white.png" alt=""></div>+38
-                                    068 005 3570
+                                    <div class="menu_contact_icon"><img src="images/phone_white.png" alt=""></div>+38 068 005 3570
                                 </div>
                                 <div class="menu_contact_item">
-                                    <div class="menu_contact_icon"><img src="images/mail_white.png" alt=""></div><a
-                                        href="mailto:fastsales@gmail.com">fastsales@gmail.com</a>
+                                    <div class="menu_contact_icon"><img src="images/mail_white.png" alt=""></div><a href="mailto:fastsales@gmail.com">fastsales@gmail.com</a>
                                 </div>
                             </div>
                         </div>
