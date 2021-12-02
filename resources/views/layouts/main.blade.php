a57da5d3a330c983ad4b6c3d0ff74c6b8e571803
Trivena Panjaitan <70930454+TrivenaPanjaitan@users.noreply.github.com>
2021-12-02 15:14:38 +0700

    update header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,7 +23,6 @@
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
