2a51dab99f434598a13764d7c24f6fbbd893690c
TrivenaPanjaitan <trivenapanjaitan7.1.3@gmail.com>
2021-12-08 17:17:46 +0700

    update beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -173,6 +173,95 @@
     </div>
     <!-- end produk promo -->
 
+    <!-- produk Terbaru-->
+    <div class="row mt-4">
+        <div class="col col-md-12 col-sm-12 mb-4">
+            <h2 class="text-center">Terbaru</h2>
+        </div>
+        <!-- produk pertama -->
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+                <a href="{{ URL::to('produk/satu') }}">
+                    <img src="{{ asset('images/slide2.jpg') }}" alt="foto produk" class="card-img-top">
+                </a>
+                <div class="card-body">
+                    <a href="{{ URL::to('produk/satu') }}" class="text-decoration-none">
+                        <p class="card-text">
+                            Produk Pertama
+                        </p>
+                    </a>
+                    <div class="row mt-4">
+                        <div class="col">
+                            <button class="btn btn-light">
+                                <i class="far fa-heart"></i>
+                            </button>
+                        </div>
+                        <div class="col-auto">
+                            <p>
+                                Rp. 10.000,00
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- produk kedua -->
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+                <a href="{{ URL::to('produk/dua') }}">
+                    <img src="{{ asset('images/slide2.jpg') }}" alt="foto produk" class="card-img-top">
+                </a>
+                <div class="card-body">
+                    <a href="{{ URL::to('produk/dua') }}" class="text-decoration-none">
+                        <p class="card-text">
+                            Produk Kedua
+                        </p>
+                    </a>
+                    <div class="row mt-4">
+                        <div class="col">
+                            <button class="btn btn-light">
+                                <i class="far fa-heart"></i>
+                            </button>
+                        </div>
+                        <div class="col-auto">
+                            <p>
+                                Rp. 10.000,00
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- produk ketiga -->
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+                <a href="{{ URL::to('produk/tiga') }}">
+                    <img src="{{ asset('images/slide2.jpg') }}" alt="foto produk" class="card-img-top">
+                </a>
+                <div class="card-body">
+                    <a href="{{ URL::to('produk/tiga') }}" class="text-decoration-none">
+                        <p class="card-text">
+                            Produk Ketiga
+                        </p>
+                    </a>
+                    <div class="row mt-4">
+                        <div class="col">
+                            <button class="btn btn-light">
+                                <i class="far fa-heart"></i>
+                            </button>
+                        </div>
+                        <div class="col-auto">
+                            <p>
+                                Rp. 10.000,00
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end produk terbaru -->
+
     <div class="row my-4 justify-content-center pt-5">
         <div class="col-12 col-sm-12 col-md-8">
 
