29184f8d567c8b3ab1bcb36ae833a2dedc2573a2
ifs19017 <ifs19017@students.del.ac.id>
2021-12-10 00:47:19 +0700

    update registercontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,7 +70,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'phone' => $data['phone'], //tambahan kolom phone (no tlp)
+            // 'phone' => $data['phone'], //tambahan kolom phone (no tlp)
 
         ]);
     }
